5dac6a76509dfa6456448045e0a28857f5e52ab9
Paolo Ráez <str237@gmail.com>
2023-10-08 11:26:20 +0200

    Health endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Elysia } from "elysia";
+import { HealthController } from "./controllers";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().use(HealthController).listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
