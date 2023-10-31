e8fe780f2f6ba9ca945559af3936fc1b95ad2c73
Paolo Ráez <str237@gmail.com>
2023-10-31 23:53:19 +0100

    Re-structure

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,12 +1,19 @@
 import Elysia from 'elysia'
-import { UserModel } from '../models'
+import type { BaseApp } from '../main'
 
-export const UsersController = new Elysia({ prefix: '/users' })
-  .use(UserModel)
-  .post('/', async ctx => {}, {
-    body: 'user.create-body',
-  })
-  .get('/:id', async ctx => {})
-  .get('/wip', async ctx => {
-    return 'wip'
-  })
+export const UsersController = (app: BaseApp) =>
+  app
+    .post(
+      '/',
+      async ctx => {
+        console.log('this')
+        console.log(ctx.store.a)
+      },
+      {
+        body: 'user.create-body',
+      }
+    )
+    .get('/:id', async ctx => {})
+    .get('/wip', async ctx => {
+      return 'wip'
+    })
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { Elysia } from 'elysia'
 import { HealthController, UsersController } from './controllers'
+import { UserModel } from './models'
 
-const api = new Elysia({
-  prefix: '/api/v1',
-}).use(UsersController)
+const createBaseApp = () => {
+  return new Elysia().state({ a: 1 }).use(UserModel)
+}
+
+export type BaseApp = ReturnType<typeof createBaseApp>
 
 export const createApp = () => {
-  return new Elysia().use(HealthController).use(api)
+  return createBaseApp()
+    .use(HealthController)
+    .group('/api/v1/users', app => app.use(UsersController))
 }
 
 export type App = ReturnType<typeof createApp>
